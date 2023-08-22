640e04b0236c5363bcbf69edaac25291341b257e
Esteban Muchenvicz <105963359+Estebanmuchenvicz@users.noreply.github.com>
2023-08-22 03:36:59 -0300

    Update vite.config.js

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [ /*WindiCSS(),*/react()],
+  exclude: ['**/*.glb'] // Excluir archivos .glb del análisis de dependencias
 })
